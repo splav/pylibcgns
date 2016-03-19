f7f9c660bf42e461e4a06bb33fb5d45f09065524
Alexandrov Sergey <splavgm@gmail.com>
2016-03-20 00:09:26 +0300

    fix windows includes

--- setup.py
+++ setup.py
@@ -3,17 +3,26 @@ from distutils.extension import Extension
 from Cython.Distutils import build_ext
 import numpy as np
 
-ext_modules = [Extension("pylibcgns",
-                     ["pylibcgns.pyx"],
+import platform
+
+include_dirs = [np.get_include()]
+library_dirs = []
+if platform.system() == "Windows":
+    library_dirs += [r"C:\Program Files\cgns_15\lib"]
+    include_dirs += [r"C:\Program Files\cgns_15\include"]
+
+ext_modules = [Extension("pycgns",
+                     ["pycgns.pyx"],
                      language='c',
                      libraries=["cgns", "hdf5"],
-                     include_dirs=[np.get_include()],
+                     library_dirs=library_dirs,
+                     include_dirs=include_dirs,
                      )]
 
 setup(
   name = 'pylibcgns',
   cmdclass = {'build_ext': build_ext},
-  version='0.0.3',
+  version='0.0.4',
 
   author='Aleksandrov Sergey',
   author_email='splavgm@gmail.com',
