aaeabb94e56772d381847fe7fc256fece13b03a3
Alexandrov Sergey <splavgm@gmail.com>
2016-03-19 11:33:21 +0300

    fix numpy header autodetect

--- setup.py
+++ setup.py
@@ -1,17 +1,19 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+import numpy as np
 
 ext_modules = [Extension("pylibcgns",
                      ["pylibcgns.pyx"],
                      language='c',
                      libraries=["cgns", "hdf5"],
+                     include_dirs=[np.get_include()],
                      )]
 
 setup(
   name = 'pylibcgns',
   cmdclass = {'build_ext': build_ext},
-  version='0.0.2',
+  version='0.0.3',
 
   author='Aleksandrov Sergey',
   author_email='splavgm@gmail.com',
